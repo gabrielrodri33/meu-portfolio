3c9777ce7eb0e15c1e5867e00807a51acf2e728a
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-10-20 19:16:16 -0300

    Estrutura básica

---
+++ src/app/portfolio/not-found.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+        <h1>OOPS! 404!</h1>
+        <p>Página não encontrada!</p>
+    </div>
+  )
+}
---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Cabecalho() {
+  return (
+    <div className="cabecalho">
+      <h1>Cabecalho</h1>
+    </div>
+  )
+}
