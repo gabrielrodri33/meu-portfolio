9ff72602579af79c73c9269a716d1c95e6cd1dd8
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-10-20 21:46:41 -0300

    Criado e inicio no style footer e header

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-    <div className="cabecalho">
-      <h1>Cabecalho</h1>
-    </div>
-  )
+    <header className="cabecalho">
+      <nav>
+        <ul>
+          <li className="name">
+            <Link href="/">Gabriel S.</Link>
+          </li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/sobre">Sobre</Link>
+          </li>
+          <li>
+            <Link href="/projetos">Projetos</Link>
+          </li>
+          <li>
+            <Link href="https://www.linkedin.com/in/gabrielrodri/" target="_blank">Linkedin</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Rodape() {
   return (
-    <div>Rodape</div>
+    <div className='rodape'>
+      <p>Desenvolvido por <span>Gabriel Siqueira</span></p>
+      <p>NEXT.js - React.js - Tailwind</p>
+    </div>
   )
 }
